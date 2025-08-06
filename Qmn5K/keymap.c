9cda1fec4f47b9317f5037d0cbcf50d2c3c76fe4
NOC <noconnor@movingintelligence.de>
2025-08-06 13:17:54 +0200

    Add housekeeping task for achordion

--- Qmn5K/keymap.c
+++ Qmn5K/keymap.c
@@ -164,6 +164,9 @@ bool rgb_matrix_indicators_user(void) {
   return true;
 }
 
+void housekeeping_task_user(void) {
+  achordion_task();
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (!process_achordion(keycode, record)) { return false; }
